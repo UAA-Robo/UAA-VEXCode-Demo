9396c360c59107f912e69c05ddf8c53704cb21ea
rjmichaelii <rjmichaelii@alaska.edu>
2023-07-06 21:09:35 -0800

    Reformatted to camelCase

--- include/Hardware.h
+++ include/Hardware.h
@@ -1,38 +1,38 @@
 #pragma once
 #include "vex.h"
 
-class Hardware{
+class Hardware
+{
 
 public:
-
     Hardware();
 
     vex::brain brain;
     vex::controller controller = vex::controller(vex::primary);
 
     // Drivetrain
-    vex::motor wheel_front_left = vex::motor(vex::PORT1, vex::ratio18_1, false);
-    vex::motor wheel_front_right = vex::motor(vex::PORT9, vex::ratio18_1, true);
-    vex::motor wheel_back_left = vex::motor(vex::PORT2, vex::ratio18_1, true);
-    vex::motor wheel_back_right = vex::motor(vex::PORT10, vex::ratio18_1, false);
+    vex::motor wheelFrontLeft = vex::motor(vex::PORT1, vex::ratio18_1, false);
+    vex::motor wheelFrontRight = vex::motor(vex::PORT9, vex::ratio18_1, true);
+    vex::motor wheelBackLeft = vex::motor(vex::PORT2, vex::ratio18_1, true);
+    vex::motor wheelBackRight = vex::motor(vex::PORT10, vex::ratio18_1, false);
 
-    vex::motor_group left_wheels = vex::motor_group(wheel_front_left, wheel_back_left);
-    vex::motor_group right_wheels = vex::motor_group(wheel_front_right, wheel_back_right);
+    vex::motor_group leftWheels = vex::motor_group(wheelFrontLeft, wheelBackLeft);
+    vex::motor_group rightWheels = vex::motor_group(wheelFrontRight, wheelBackRight);
 
-    vex::motor_group drivetrain = vex::motor_group(wheel_front_left, wheel_back_left, wheel_front_right, wheel_back_right);
+    vex::motor_group drivetrain = vex::motor_group(wheelFrontLeft, wheelBackLeft, wheelFrontRight, wheelBackRight);
 
-    //Disk launcher (flywheel and flicker)
+    // Disk launcher (flywheel and flicker)
     vex::motor flywheel_top = vex::motor(vex::PORT4, vex::ratio6_1, false);
     vex::motor flywheel_bottom = vex::motor(vex::PORT5, vex::ratio6_1, true);
-    vex::motor_group flywheel = vex::motor_group(flywheel_top, flywheel_bottom); 
+    vex::motor_group flywheel = vex::motor_group(flywheel_top, flywheel_bottom);
 
-    vex::motor disk_flicker = vex::motor(vex::PORT3, vex::ratio18_1, false);   
+    vex::motor disk_flicker = vex::motor(vex::PORT3, vex::ratio18_1, false);
 
     // Intake motors
-    vex::motor intake_bottoom = vex::motor(vex::PORT6, vex::ratio6_1, false);
-    vex::motor intake_middle_left = vex::motor(vex::PORT7, vex::ratio6_1, true);
-    vex::motor intake_middleR_right = vex::motor(vex::PORT8, vex::ratio6_1, false);
-    vex::motor intake_top = vex::motor(vex::PORT20, vex::ratio6_1, true);
+    vex::motor intakeBottom = vex::motor(vex::PORT6, vex::ratio6_1, false);
+    vex::motor intakeMiddleLeft = vex::motor(vex::PORT7, vex::ratio6_1, true);
+    vex::motor intakeMiddleRight = vex::motor(vex::PORT8, vex::ratio6_1, false);
+    vex::motor intakeTop = vex::motor(vex::PORT20, vex::ratio6_1, true);
 
-    vex::motor_group intake = vex::motor_group(intake_bottoom, intake_middle_left, intake_middleR_right, intake_top); //Use this
+    vex::motor_group intake = vex::motor_group(intakeBottom, intakeMiddleLeft, intakeMiddleRight, intakeTop); // Use this
 };
