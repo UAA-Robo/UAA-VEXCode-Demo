fd9d49779742d4399831cea0d83de946705847da
Jaren Ramirez <jarenramirez2019@gmail.com>
2023-07-06 20:32:49 -0800

    Reformatted drive.h and drive.cpp to fit to the new standards

--- include/Drive.h
+++ include/Drive.h
@@ -4,36 +4,13 @@
 
 class Drive{
 
-
 public:
-    virtual void drive(){}
-
-    /// @brief Outputs distance from robot to goal to computer screen
-    double outputDistanceToGoal();
-
-    void flyweelControlswPID();
-
 
 protected:
+
     Drive(Hardware* hardware);
 
     Hardware* hw;
-
-    
-
-    /// @brief      Calculates the velocity in RPMs that the left and right drivetrain wheels should recieve based on
-    ///             the horizontal percentage and vertical percentage passed in. Automatically scales the velocities
-    ///             for the drivetrain gear inserts.
-    /// @param velPercent   Pair of doubles {verticalVelocityPercent, horizontalVelocityPercent} from -100 to 100 
-    ///                     that represent the percentage that the drivetrain should move forward/backward and left/right.
-    ///                     For example of verticalVelocityPercent = 50  and horizontalVelocityPercent = 0, the bot 
-    ///                     will move forward at 50% velocity. Likewise if verticalVelocityPercent = 0  and 
-    ///                     horizontalVelocityPercent = 50, the drivetrain will rotate to the right at 50% velocity.
-    ///                     Any combiniatiion of non-zero verticalVelocityPercents and horizontalVelocityPercents 
-    ///                     will cause the drivetrain to move in a arc.
-    /// @return     Returns a pair of doubles {leftWheelsVelocity, rightWheelsVelocity} that represent the actual velocities in RPM 
-    ///             (scaled to the gear-ratio) that the wheels on each drivetrain side need to be set to in order move as expected. 
-    std::pair<double,double>  calculateDriveTrainVel(std::pair<double,double> velPercent); 
 
     /// @brief       Moves the drivetrain based on the horizontal percentage and vertical percentage passed in. Calls calculateDriveTrainVel
     ///             to convert those percentages to actual velocites in RPM.
@@ -44,39 +21,19 @@ protected:
     ///                     horizontalVelocityPercent = 50, the drivetrain will rotate to the right at 50% velocity.
     ///                     Any combiniatiion of non-zero verticalVelocityPercents and horizontalVelocityPercents 
     ///                     will cause the drivetrain to move in a arc.
-    void moveDriveTrain(std::pair<double,double> velPercent);
-
-    /// @brief      Moves the drivetrain a cetain distance based on the horizontal percentage and vertical percentage passed in. Calls calculateDriveTrainVel
-    ///             to convert those percentages to actual velocites in RPM.
-    /// @param velPercent   Pair of doubles {verticalVelocityPercent, horizontalVelocityPercent} from -100 to 100 
-    ///                     that represent the percentage that the drivetrain should move forward/backward and left/right.
-    ///                     For example of verticalVelocityPercent = 50  and horizontalVelocityPercent = 0, the bot 
-    ///                     will move forward at 50% velocity. Likewise if verticalVelocityPercent = 0  and 
-    ///                     horizontalVelocityPercent = 50, the drivetrain will rotate to the right at 50% velocity.
-    ///                     Any combiniatiion of non-zero verticalVelocityPercents and horizontalVelocityPercents 
-    ///                     will cause the drivetrain to move in a arc.
-    /// @param distance     Distance in inches to move.
-    void moveDriveTrainDistance(std::pair<double,double> velPercent, double distance);
+    void move_drive_train(std::pair<double,double> velPercent);
 
     /// @brief  Spins the intake indefinitely unless ISSTOP is passed as true.
     /// @param ISSTOP       If true, stops the intake.
     /// @param ISINVERT     If true, runs the intake in reverse;
-    void spinIntake(bool ISSTOP = false, bool ISINVERT = false, int volts=12);
-
-    //void (Drive::*spinIntakePtr)(bool, bool) = &Drive::spinIntake;
+    void spin_intake(bool ISSTOP = false, bool ISINVERT = false, int volts=12);
 
     /// @brief  Sets the voltage and spins the flywheel until the function is called again with a differrent voltage (like 0).
     /// @param voltage      Double from -12,000 to 12,000. Negative voltage reversed the flywheel.
-    void spinFlywheel(double voltage);
+    void spin_flywheel(double voltage);
 
     /// @brief Flicks the disk by moving the launcer/flicker forward and backward once.
-    void flickDisk();
+    void flick_disk();
 
-    /// @brief  Expands the bot by launching string by pulling the pins from 3 catapults.
-    void expand();
-    
-    /// @brief TODO: NOT implemented yet!!!!
-    void errorCorrect();
-    
-
+    std::pair<double,double> calculate_drive_train_velocity(std::pair<double,double> velPercent);
 };
--- src/Drive.cpp
+++ src/Drive.cpp
@@ -6,16 +6,15 @@ Drive::Drive(Hardware* hardware) {
 }
 
 
-void Drive::moveDriveTrain(std::pair<double,double> velPercent)
+void Drive::move_drive_train(std::pair<double,double> velPercent)
 {
-    std::pair<double,double> vel = calculateDriveTrainVel(velPercent);
-
+    std::pair<double,double> vel = calculate_drive_train_velocity(velPercent);
     hw->leftWheels.spin(vex::directionType::fwd, vel.first, vex::velocityUnits::pct);
     hw->rightWheels.spin(vex::directionType::fwd, vel.second, vex::velocityUnits::pct);
 }
 
 
-void Drive :: spinIntake(bool ISSTOP, bool ISINVERT, int volts) {
+void Drive :: spin_intake(bool ISSTOP, bool ISINVERT, int volts) {
     if (ISSTOP) {
         hw->intake.stop();
     }else {
@@ -27,7 +26,8 @@ void Drive :: spinIntake(bool ISSTOP, bool ISINVERT, int volts) {
     }
 }
 
-void Drive :: spinFlywheel(double voltage) {
+
+void Drive :: spin_flywheel(double voltage) {
     if(voltage < 0){
         hw->flywheel.spin(vex::directionType::rev, -voltage, vex::voltageUnits::volt);
     }else{
@@ -36,8 +36,7 @@ void Drive :: spinFlywheel(double voltage) {
 }
 
 
-void Drive :: flickDisk() {
-
+void Drive :: flick_disk() {
     hw->launcher.spin(vex::forward,7,vex::volt);
     wait(150,vex::msec);
     hw->launcher.spin(vex::reverse,8,vex::volt);
@@ -45,3 +44,26 @@ void Drive :: flickDisk() {
     hw->launcher.stop();
 }
 
+
+std::pair<double,double> Drive::calculate_drive_train_velocity(std::pair<double,double> velPercent) //{verticalVelPercent, horizontalVelPercent}
+{
+        double verticalVelPercent = velPercent.first / 100;
+        double horizontalVelPercent = velPercent.second / 100;
+
+        // Calculate raw left and right motor velocity
+        double rawLeftVel = verticalVelPercent + horizontalVelPercent; //raw velocity of left wheels in drive train
+        double rawRightVel = verticalVelPercent - horizontalVelPercent; //raw velocity of right wheels in drive train
+        
+        // Normalize the motor velocity
+        double maxRawVel = std::max(std::abs(rawLeftVel), std::abs(rawRightVel));
+        double normalizationFactor = maxRawVel > 1.0 ? maxRawVel : 1.0;
+
+        double leftVelMultiplier = rawLeftVel / normalizationFactor;
+        double rightVelMultiplier = rawRightVel / normalizationFactor;
+
+
+        double leftVel =  100 * leftVelMultiplier; 
+        double rightVel =  100 * rightVelMultiplier;
+
+        return {leftVel, rightVel};
+}
