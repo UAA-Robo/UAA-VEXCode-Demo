8ec4e760a7d96ab8c49161effb6eedfd49d38a98
rjmichaelii <rjmichaelii@alaska.edu>
2023-07-06 21:30:26 -0800

    Reformatted Driver.cpp to camelCase

--- src/Drive.cpp
+++ src/Drive.cpp
@@ -1,69 +1,75 @@
 #include "Drive.h"
 
-Drive::Drive(Hardware* hardware) {
+Drive::Drive(Hardware *hardware)
+{
     hw = hardware;
-
 }
 
-
-void Drive::move_drive_train(std::pair<double,double> velPercent)
+void Drive::moveDriveTrain(std::pair<double, double> velPercent)
 {
-    std::pair<double,double> vel = calculate_drive_train_velocity(velPercent);
+    std::pair<double, double> vel = calculateDriveTrainVelocity(velPercent);
     hw->leftWheels.spin(vex::directionType::fwd, vel.first, vex::velocityUnits::pct);
     hw->rightWheels.spin(vex::directionType::fwd, vel.second, vex::velocityUnits::pct);
 }
 
-
-void Drive :: spin_intake(bool ISSTOP, bool ISINVERT, int volts) {
-    if (ISSTOP) {
+void Drive ::spinIntake(bool ISSTOP, bool ISINVERT, int volts)
+{
+    if (ISSTOP)
+    {
         hw->intake.stop();
-    }else {
-        if(ISINVERT){
+    }
+    else
+    {
+        if (ISINVERT)
+        {
             hw->intake.spin(vex::directionType::rev, volts, vex::voltageUnits::volt);
-        }else{
+        }
+        else
+        {
             hw->intake.spin(vex::directionType::fwd, volts, vex::voltageUnits::volt);
         }
     }
 }
 
-
-void Drive :: spin_flywheel(double voltage) {
-    if(voltage < 0){
+void Drive ::spinFlywheel(double voltage)
+{
+    if (voltage < 0)
+    {
         hw->flywheel.spin(vex::directionType::rev, -voltage, vex::voltageUnits::volt);
-    }else{
+    }
+    else
+    {
         hw->flywheel.spin(vex::directionType::fwd, voltage, vex::voltageUnits::volt);
     }
 }
 
-
-void Drive :: flick_disk() {
-    hw->launcher.spin(vex::forward,7,vex::volt);
-    wait(150,vex::msec);
-    hw->launcher.spin(vex::reverse,8,vex::volt);
-    wait(200,vex::msec);
-    hw->launcher.stop();
+void Drive ::flickDisk()
+{
+    hw->diskFlicker.spin(vex::forward, 7, vex::volt);
+    wait(150, vex::msec);
+    hw->diskFlicker.spin(vex::reverse, 8, vex::volt);
+    wait(200, vex::msec);
+    hw->diskFlicker.stop();
 }
 
-
-std::pair<double,double> Drive::calculate_drive_train_velocity(std::pair<double,double> velPercent) //{verticalVelPercent, horizontalVelPercent}
+std::pair<double, double> Drive::calculateDriveTrainVelocity(std::pair<double, double> velPercent) //{verticalVelPercent, horizontalVelPercent}
 {
-        double verticalVelPercent = velPercent.first / 100;
-        double horizontalVelPercent = velPercent.second / 100;
+    double verticalVelPercent = velPercent.first / 100;
+    double horizontalVelPercent = velPercent.second / 100;
 
-        // Calculate raw left and right motor velocity
-        double rawLeftVel = verticalVelPercent + horizontalVelPercent; //raw velocity of left wheels in drive train
-        double rawRightVel = verticalVelPercent - horizontalVelPercent; //raw velocity of right wheels in drive train
-        
-        // Normalize the motor velocity
-        double maxRawVel = std::max(std::abs(rawLeftVel), std::abs(rawRightVel));
-        double normalizationFactor = maxRawVel > 1.0 ? maxRawVel : 1.0;
+    // Calculate raw left and right motor velocity
+    double rawLeftVel = verticalVelPercent + horizontalVelPercent;  // raw velocity of left wheels in drive train
+    double rawRightVel = verticalVelPercent - horizontalVelPercent; // raw velocity of right wheels in drive train
 
-        double leftVelMultiplier = rawLeftVel / normalizationFactor;
-        double rightVelMultiplier = rawRightVel / normalizationFactor;
+    // Normalize the motor velocity
+    double maxRawVel = std::max(std::abs(rawLeftVel), std::abs(rawRightVel));
+    double normalizationFactor = maxRawVel > 1.0 ? maxRawVel : 1.0;
 
+    double leftVelMultiplier = rawLeftVel / normalizationFactor;
+    double rightVelMultiplier = rawRightVel / normalizationFactor;
 
-        double leftVel =  100 * leftVelMultiplier; 
-        double rightVel =  100 * rightVelMultiplier;
+    double leftVel = 100 * leftVelMultiplier;
+    double rightVel = 100 * rightVelMultiplier;
 
-        return {leftVel, rightVel};
+    return {leftVel, rightVel};
 }
